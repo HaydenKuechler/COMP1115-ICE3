163ed6aebfd492a9883f0d8e64328858a6b6c8c6
HaydenKuechler <haydenkue.business@gmail.com>
2025-09-19 08:36:43 -0400

    Add project files.

---
+++ ICE3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ICE3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+            Console.Title = "Character Sheet";
+            String Character;
+
+            //Primary attributes 
+            int Agility;
+            int Strength;
+            int Vigor;
+            int Perception;
+            int Intellect;
+            int Will;
+            //Secondary Atributes 
+            int Awareness;
+            int toughness;
+            int resolve;
+            //Prompt Character name 
+            Console.WriteLine("Please Enter your Name");
+            string? CharcterName = Console.ReadLine();
+            Character = Console.ReadLine();
+
+        }
+    }
+}
