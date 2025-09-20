04e944ed66a5d9b84f6912cce38ab5ee4f72c7f9
HaydenKuechler <haydenkue.business@gmail.com>
2025-09-19 22:46:55 -0400

    Added Menu Option 6 for random career

--- ICE3/Program.cs
+++ ICE3/Program.cs
@@ -44,10 +44,10 @@ using System.Security.Cryptography;
     Console.WriteLine("6 Assassain AGL 45, STR 25, VGR 25, PER 35, INT, 35 Will 25)");
     Console.WriteLine("7 Random");
     CareerChoice = Convert.ToInt32(Console.ReadLine());
+    //Added switch case to select career and assign attributes
+    switch (CareerChoice)
     {
-        //Choices and their stats
-        if (CareerChoice == 1)
-        {
+        case 1:
             Career = "Army";
             Agility = 35;
             Strength = 40;
@@ -55,12 +55,10 @@ using System.Security.Cryptography;
             Perception = 25;
             Intellect = 35;
             Will = 30;
-            Console.WriteLine("Welcome to camp Solider!");
             HasChosenCareer = true;
+            break;
 
-        }
-        else if (CareerChoice == 2)
-        {
+        case 2:
             Career = "Psion";
             Agility = 25;
             Strength = 25;
@@ -68,24 +66,19 @@ using System.Security.Cryptography;
             Perception = 35;
             Intellect = 35;
             Will = 30;
-            Console.WriteLine("ahh one with the Psycics!");
-
-        }
-
-        else if (CareerChoice == 3)
-        {
+            HasChosenCareer = true;
+            break;
+        case 3:
             Career = "Rouge";
             Agility = 30;
             Strength = 25;
             Vigor = 30;
             Perception = 20;
-            Intellect = 35;
+            Intellect = 25;
             Will = 30;
-            Console.WriteLine("You don't listen to others well do you?");
-
-        }
-        else if (CareerChoice == 4)
-        {
+            HasChosenCareer = true;
+            break;
+        case 4:
             Career = "Telepath";
             Agility = 30;
             Strength = 25;
@@ -93,11 +86,9 @@ using System.Security.Cryptography;
             Perception = 30;
             Intellect = 40;
             Will = 30;
-            Console.WriteLine("A mind reader you say I better stop thinking");
-
-        }
-        else if (CareerChoice == 5)
-        {
+            HasChosenCareer = true;
+            break;
+        case 5:
             Career = "Tinker";
             Agility = 25;
             Strength = 30;
@@ -105,91 +96,68 @@ using System.Security.Cryptography;
             Perception = 30;
             Intellect = 30;
             Will = 25;
-            Console.WriteLine("Lets get to work!");
-
-        }
-        else if (CareerChoice == 6)
-        {
-            Career = "Assassin";
+            HasChosenCareer = true;
+            break;
+        case 6:
+            Career = "Assassain";
             Agility = 45;
             Strength = 25;
             Vigor = 25;
             Perception = 35;
             Intellect = 35;
             Will = 25;
-            Console.WriteLine("Cold but Calculated!");
-
-        }
-        else if (CareerChoice == 7)
-        {
-            Console.WriteLine("Okay so random we go");
-            Random random = new Random();
-            Console.WriteLine(random.Next(1, 7));
-            Console.WriteLine($"Your random number is:{random.Next(7) + 1}");
-
-        }
-        else if (CareerChoice == 8)
-        {
+            HasChosenCareer = true;
+            break;
+        case 7:
+            Career = "Random";
+            Random rand = new Random();
+            Agility = rand.Next(5, 51);
+            Strength = rand.Next(5, 51);
+            Vigor = rand.Next(5, 51);
+            Perception = rand.Next(5, 51);
+            Intellect = rand.Next(5, 51);
+            Will = rand.Next(5, 51);
+            HasChosenCareer = true;
+            break;
+        default:
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Please select a Class Provided");
-            Console.WriteLine();
-            Console.WriteLine();
-        }
-        if (HasChosenCareer == true)
-        
-            Console.WriteLine("Welcome to the World Traveler");
-            Console.Clear();
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("---------------------------Character Stat Menu----------------------------");
-            Console.WriteLine("--------------------------------------------------------------------------");
-            Console.WriteLine("---------------------------" + CharacterName + "--------------------------");
-            Console.WriteLine("Career" + "---------------" + Career + "----------------------------------");
-            Console.WriteLine("Primary Attributes" + "---------------------------------------------------");
-            Console.WriteLine("--------------------------------------------------------------------------");
-            Console.WriteLine($"Agility----------------------={Agility}----------------------------------");
-            Console.WriteLine($"Strength---------------------={Strength}---------------------------------");
-            Console.WriteLine($"Vigor------------------------={Vigor}------------------------------------");
-            Console.WriteLine($"Perception-------------------={Perception}-------------------------------");
-            Console.WriteLine($"Intellect--------------------={Intellect}--------------------------------");
-            Console.WriteLine($"Will-------------------------={Will}-------------------------------------");
-            Console.WriteLine("--------------------------------------------------------------------------");
-            Console.WriteLine("Secondary Attributes" + "-------------------------------------------------");
-            Console.WriteLine("--------------------------------------------------------------------------");
-            Console.WriteLine($"Awareness--------------------={Awareness}--------------------------------");
-            Console.WriteLine($"Toughness--------------------={Toughness}--------------------------------");
-            Console.WriteLine($"Resolve----------------------={Resolve}----------------------------------");
-            Console.WriteLine("--------------------------------------------------------------------------");
-            Console.WriteLine("Press Any Key to Continue"+"----------------------------------------------");    
-        Console.ReadKey(true);
-        
-
-        { }
-        }
-
-
-
-        /* Console.Clear(); Console.BackgroundColor = ConsoleColor.DarkCyan;
-         Console.ForegroundColor = ConsoleColor.Black;
-         Console.WriteLine("Welcome To the Game");
-         Console.WriteLine("-------------------------------------------------------------");
-        */
-
-
-
-
-
+            Console.WriteLine("Invalid Choice, Please choose a career");
+            break;
     }
+    //Added If statement to check if a career has been chosen
+    if (HasChosenCareer == true) Console.WriteLine("Welcome to the World Traveler");
+    Console.Clear();
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("---------------------------Character Stat Menu----------------------------");
+    Console.WriteLine("--------------------------------------------------------------------------");
+    Console.WriteLine("---------------------------" + CharacterName + "--------------------------");
+    Console.WriteLine("Career" + "---------------" + Career + "----------------------------------");
+    Console.WriteLine("Primary Attributes" + "---------------------------------------------------");
+    Console.WriteLine("--------------------------------------------------------------------------");
+    Console.WriteLine($"Agility----------------------={Agility}----------------------------------");
+    Console.WriteLine($"Strength---------------------={Strength}---------------------------------");
+    Console.WriteLine($"Vigor------------------------={Vigor}------------------------------------");
+    Console.WriteLine($"Perception-------------------={Perception}-------------------------------");
+    Console.WriteLine($"Intellect--------------------={Intellect}--------------------------------");
+    Console.WriteLine($"Will-------------------------={Will}-------------------------------------");
+    Console.WriteLine("--------------------------------------------------------------------------");
+    Console.WriteLine("Secondary Attributes" + "-------------------------------------------------");
+    Console.WriteLine("--------------------------------------------------------------------------");
+    //Calculating Secondary Attributes
+    Awareness = Perception + Intellect;
+    Toughness = Vigor + Strength;
+    Resolve = Will + Intellect;
+    Console.WriteLine($"Awareness--------------------={Awareness}--------------------------------");
+    Console.WriteLine($"Toughness--------------------={Toughness}--------------------------------");
+    Console.WriteLine($"Resolve----------------------={Resolve}----------------------------------");
+    Console.WriteLine("--------------------------------------------------------------------------");
+    Console.WriteLine("Press Any Key to Continue" + "----------------------------------------------");
+    Console.ReadKey(true);
 
-    {
-
-
-
-
-
-    }
+}
 
 
 
